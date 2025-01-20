3c2d90671debc6e02fcf0fa3eeb8c0bbb1f55f19
Pynmash <157254317+pynmash@users.noreply.github.com>
2025-01-20 11:06:28 +0000

    Hide secrets

--- main.py
+++ main.py
@@ -9,13 +9,18 @@ from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text, ForeignKey
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import load_dotenv
+import os
 
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
+load_dotenv()
+FLASK_KEY = os.environ.get("FLASK_KEY")
+
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+app.config["SECRET_KEY"] = FLASK_KEY
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 app.config["CKEDITOR_CONFIG"] = {"versionCheck": False}
@@ -49,7 +54,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -236,4 +241,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False)
